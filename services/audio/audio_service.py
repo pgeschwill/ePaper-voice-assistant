3db0b382a988cd74f8fc75fdede77fd9e6e58ac0
Pascal <pascal.geschwill@hotmail.de>
2024-03-13 20:44:39 +0100

    Replace system calls for audio service with pyalsaaudio

--- services/audio/audio_service.py
+++ services/audio/audio_service.py
@@ -5,15 +5,19 @@ import os
 import re
 import subprocess
 import requests
+import wave
+import alsaaudio
+import json
 
 app = Flask(__name__)
+
+with open("/config/config.json") as config_file:
+   config = json.load(config_file)
+
+PLAYBACK_DEVICE_NAME = config["audio"]["playback_device_name"]
 PATH_TO_THIS_FILE = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_RESPONSE_FILES = os.path.join(PATH_TO_THIS_FILE, "wav")
 MIMIC3_URL = "http://mimic3:59125/api/tts"
-OS_COMMANDS = {
-    "set_output_volume": "amixer",
-    "play_wav": "aplay"
-}
 
 @app.route("/health", methods=['GET'])
 def health():
@@ -27,8 +31,7 @@ def play_response():
     num_response_files = len(response_files)
     file_index_to_play = randrange(0, num_response_files)
     file_name_to_play = os.path.join(PATH_TO_RESPONSE_FILES, response_files[file_index_to_play])
-    command = [OS_COMMANDS["play_wav"], file_name_to_play]
-    subprocess.run(command)
+    play(file_name_to_play)
     return jsonify(success=True)
 
 @app.route("/generate_response", methods=['GET'])
@@ -43,8 +46,7 @@ def generate_response():
     response = requests.get(MIMIC3_URL, params = params)
     with open(output_filename, "wb") as out_file:
         out_file.write(response.content)
-    play_command = [OS_COMMANDS["play_wav"], output_filename]
-    subprocess.run(play_command)
+    play(output_filename)
     return jsonify(success=True)
 
 @app.route("/play_wav", methods=['GET'])
@@ -53,8 +55,7 @@ def play_wav():
     full_filename = f"{os.path.join(PATH_TO_RESPONSE_FILES, filename)}.wav"
     if not os.path.exists(full_filename):
         return jsonify(success=False)
-    command = [OS_COMMANDS["play_wav"], full_filename]
-    subprocess.run(command)
+    play(full_filename)
     return jsonify(success=True)
 
 @app.route("/set_output_volume", methods=['GET'])
@@ -63,9 +64,36 @@ def set_output_volume():
     if not re.match("\d{2}", volume):
         return jsonify(success=False)
     print(f"Setting volume to {volume}")
-    command = [OS_COMMANDS["set_output_volume"], "-q", "sset", "PCM", f"{volume}%"]
-    subprocess.run(command)
+    mixer = alsaaudio.Mixer(control="PCM")
+    mixer.setvolume(int(volume))
     return jsonify(success=True)
+
+def play(filename):
+    with wave.open(filename, "rb") as f:
+
+        denominator = 1
+        # 8bit is unsigned in wav files
+        if f.getsampwidth() == 1:
+            denominator = 8
+        # Otherwise we assume signed data, little endian
+        elif f.getsampwidth() == 2:
+            denominator = 16
+        elif f.getsampwidth() == 3:
+            denominator = 24
+        elif f.getsampwidth() == 4:
+            denominator = 32
+        else:
+            raise ValueError('Unsupported format')
+
+        periodsize = f.getframerate() // denominator
+
+        device = alsaaudio.PCM(channels=f.getnchannels(), rate=f.getframerate(), format=2, periodsize=periodsize, device=PLAYBACK_DEVICE_NAME)
+        
+        while True:
+            frames = f.readframes(periodsize)
+            if not frames:
+                break
+            device.write(frames)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
