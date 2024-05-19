fe12e09f0f08aa49fe460284abbbd5380c5e6dc1
Pascal <pascal.geschwill@hotmail.de>
2024-05-19 09:34:31 +0200

    Parse rain info from forecast data

--- services/weather/weather_service.py
+++ services/weather/weather_service.py
@@ -12,6 +12,7 @@ CITY = config["weather"]["city"]
 UNITS = config["weather"]["units"]
 TIMEFORMAT = config["general"]["timeformat"]
 TIMEZONE = config["general"]["timezone"]
+NUMBER_OF_VALUES = 8
 
 @app.route("/health", methods=['GET'])
 def health():
@@ -30,7 +31,7 @@ def get_weather_data():
 def get_weather_forecast_data():
 
     url = "https://api.openweathermap.org/data/2.5/forecast"
-    response = requests.get('{}?q={}&units={}&appid={}'.format(url, CITY, UNITS, API_KEY))
+    response = requests.get('{}?q={}&units={}&appid={}&cnt='.format(url, CITY, UNITS, API_KEY, NUMBER_OF_VALUES))
     response.raise_for_status()
 
     return weather_utils.parse_weather_forecast_data(response.json())
