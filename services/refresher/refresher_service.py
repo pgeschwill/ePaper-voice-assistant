91799e49ca0935d64e05c856e2a3812619d7b347
Pascal <pascal.geschwill@hotmail.de>
2024-05-20 10:23:52 +0200

    Introduce dedicated refresh service which also pregenerates weather announcement

---
+++ services/refresher/refresher_service.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from services.audio import audio_service_wrapper as asw
+from services.infoscreen import infoscreen_service_wrapper as isw
+from time import sleep
+import json
+import requests
+
+with open("/config/config.json") as config_file:
+        config = json.load(config_file)
+    
+INFOSCREEN_SERVICE_WRAPPER = isw.InfoScreenServiceWrapper(config)
+AUDIO_SERVICE_WRAPPER = asw.AudioServiceWrapper()
+WEATHER_SERVICE_URL = "http://weather-service:8000/"
+
+def generate_weather_announcement_wav():
+    print("Generating weather announcement audio file...")
+    response = requests.get(WEATHER_SERVICE_URL + "/get_weather_announcement_text")
+    AUDIO_SERVICE_WRAPPER.generate_wav(response.content, "current_weather_info.wav")
+
+if __name__ == '__main__':
+    
+    while True:
+        now = datetime.now()
+        current_hour = int(now.strftime("%H"))
+        if current_hour == config["infoscreen"]["screensaver_hour"]:
+            print("Running screensaver...")
+            INFOSCREEN_SERVICE_WRAPPER.display_screensaver()
+        try:
+            print("Refreshing screen...")
+            INFOSCREEN_SERVICE_WRAPPER.refresh_all_panels(config)
+            generate_weather_announcement_wav()
+            
+        except Exception as e:
+            print(e)
+            AUDIO_SERVICE_WRAPPER.generate_response("Beim Aktualisieren der Infos ist ein Fehler aufgetreten.")
+        sleep(config["general"]["refresh_interval"])
