0dbaedbbf7583b2e938d9e366eae3dc6c473911d
Pascal <pascal.geschwill@hotmail.de>
2024-09-05 12:40:35 +0200

    Revert to old version of epdconfig.py because the newer version does not work with bookworm

--- services/infoscreen/driver/epdconfig.py
+++ services/infoscreen/driver/epdconfig.py
@@ -6,7 +6,7 @@
 # *----------------
 # * | This version:   V1.2
 # * | Date        :   2022-10-29
-# * | Info        :   
+# * | Info        :
 # ******************************************************************************
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documnetation files (the "Software"), to deal
@@ -31,63 +31,30 @@ import os
 import logging
 import sys
 import time
-import subprocess
 
 logger = logging.getLogger(__name__)
 
 
 class RaspberryPi:
     # Pin definition
-    RST_PIN  = 17
-    DC_PIN   = 25
-    CS_PIN   = 8
+    RST_PIN = 17
+    DC_PIN = 25
+    CS_PIN = 8
     BUSY_PIN = 24
-    PWR_PIN  = 18
+    PWR_PIN = 18
 
     def __init__(self):
         import spidev
-        import gpiozero
+        import RPi.GPIO
 
+        self.GPIO = RPi.GPIO
         self.SPI = spidev.SpiDev()
-        self.GPIO_RST_PIN    = gpiozero.LED(self.RST_PIN)
-        self.GPIO_DC_PIN     = gpiozero.LED(self.DC_PIN)
-        # self.GPIO_CS_PIN     = gpiozero.LED(self.CS_PIN)
-        self.GPIO_PWR_PIN    = gpiozero.LED(self.PWR_PIN)
-        self.GPIO_BUSY_PIN   = gpiozero.Button(self.BUSY_PIN, pull_up = False)
 
     def digital_write(self, pin, value):
-        if pin == self.RST_PIN:
-            if value:
-                self.GPIO_RST_PIN.on()
-            else:
-                self.GPIO_RST_PIN.off()
-        elif pin == self.DC_PIN:
-            if value:
-                self.GPIO_DC_PIN.on()
-            else:
-                self.GPIO_DC_PIN.off()
-        # elif pin == self.CS_PIN:
-        #     if value:
-        #         self.GPIO_CS_PIN.on()
-        #     else:
-        #         self.GPIO_CS_PIN.off()
-        elif pin == self.PWR_PIN:
-            if value:
-                self.GPIO_PWR_PIN.on()
-            else:
-                self.GPIO_PWR_PIN.off()
+        self.GPIO.output(pin, value)
 
     def digital_read(self, pin):
-        if pin == self.BUSY_PIN:
-            return self.GPIO_BUSY_PIN.value
-        elif pin == self.RST_PIN:
-            return self.RST_PIN.value
-        elif pin == self.DC_PIN:
-            return self.DC_PIN.value
-        # elif pin == self.CS_PIN:
-        #     return self.CS_PIN.value
-        elif pin == self.PWR_PIN:
-            return self.PWR_PIN.value
+        return self.GPIO.input(pin)
 
     def delay_ms(self, delaytime):
         time.sleep(delaytime / 1000.0)
@@ -99,7 +66,15 @@ class RaspberryPi:
         self.SPI.writebytes2(data)
 
     def module_init(self):
-        self.GPIO_PWR_PIN.on()
+        self.GPIO.setmode(self.GPIO.BCM)
+        self.GPIO.setwarnings(False)
+        self.GPIO.setup(self.RST_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.PWR_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
+
+        self.GPIO.output(self.PWR_PIN, 1)
 
         # SPI device, bus = 0, device = 0
         self.SPI.open(0, 0)
@@ -111,26 +86,21 @@ class RaspberryPi:
         logger.debug("spi end")
         self.SPI.close()
 
-        self.GPIO_RST_PIN.off()
-        self.GPIO_DC_PIN.off()
-        self.GPIO_PWR_PIN.off()
-
-        #self.GPIO_RST_PIN.close()
-        #self.GPIO_DC_PIN.close()
-        # self.GPIO_CS_PIN.close()
-        #self.GPIO_PWR_PIN.close()
-        #self.GPIO_BUSY_PIN.close()
-
         logger.debug("close 5V, Module enters 0 power consumption ...")
+        self.GPIO.output(self.RST_PIN, 0)
+        self.GPIO.output(self.DC_PIN, 0)
+        self.GPIO.output(self.PWR_PIN, 0)
+
+        self.GPIO.cleanup([self.RST_PIN, self.DC_PIN, self.CS_PIN, self.BUSY_PIN, self.PWR_PIN])
 
 
 class JetsonNano:
     # Pin definition
-    RST_PIN  = 17
-    DC_PIN   = 25
-    CS_PIN   = 8
+    RST_PIN = 17
+    DC_PIN = 25
+    CS_PIN = 8
     BUSY_PIN = 24
-    PWR_PIN  = 18
+    PWR_PIN = 18
 
     def __init__(self):
         import ctypes
@@ -175,9 +145,9 @@ class JetsonNano:
         self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
         self.GPIO.setup(self.PWR_PIN, self.GPIO.OUT)
         self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
-        
+
         self.GPIO.output(self.PWR_PIN, 1)
-        
+
         self.SPI.SYSFS_software_spi_begin()
         return 0
 
@@ -195,12 +165,12 @@ class JetsonNano:
 
 class SunriseX3:
     # Pin definition
-    RST_PIN  = 17
-    DC_PIN   = 25
-    CS_PIN   = 8
+    RST_PIN = 17
+    DC_PIN = 25
+    CS_PIN = 8
     BUSY_PIN = 24
-    PWR_PIN  = 18
-    Flag     = 0
+    PWR_PIN = 18
+    Flag = 0
 
     def __init__(self):
         import spidev
@@ -238,7 +208,7 @@ class SunriseX3:
             self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
 
             self.GPIO.output(self.PWR_PIN, 1)
-        
+
             # SPI device, bus = 0, device = 0
             self.SPI.open(2, 0)
             self.SPI.max_speed_hz = 4000000
@@ -260,21 +230,12 @@ class SunriseX3:
         self.GPIO.cleanup([self.RST_PIN, self.DC_PIN, self.CS_PIN, self.BUSY_PIN], self.PWR_PIN)
 
 
-
-if sys.version_info[0] == 2:
-    process = subprocess.Popen("cat /proc/cpuinfo | grep Raspberry", shell=True, stdout=subprocess.PIPE)
-else:
-    process = subprocess.Popen("cat /proc/cpuinfo | grep Raspberry", shell=True, stdout=subprocess.PIPE, text=True)
-output, _ = process.communicate()
-if sys.version_info[0] == 2:
-    output = output.decode(sys.stdout.encoding)
-
-if "Raspberry" in output:
+if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
     implementation = RaspberryPi()
 elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
     implementation = SunriseX3()
 else:
-    implementation = JetsonNano()
+    implementation = RaspberryPi()
 
 for func in [x for x in dir(implementation) if not x.startswith('_')]:
     setattr(sys.modules[__name__], func, getattr(implementation, func))
